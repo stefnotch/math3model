23589f323e5a7604aa3bfa825a21177c3098f63f
Stefnotch <brandmairstefan@gmail.com>
2025-07-31 20:00:32 +0200

    Move sample shaders

--- math3render/shaders/src/lib.rs
+++ math3render/shaders/src/lib.rs
@@ -1,4 +1,4 @@
-pub const DEFAULT_PARAMETRIC: &str = include_str!("../wgsl/DefaultParametric.wgsl");
-pub const HEART_SPHERE: &str = include_str!("../wgsl/HeartSphere.wgsl");
+pub const DEFAULT_PARAMETRIC: &str = include_str!("../wgsl/samples/DefaultParametric.wgsl");
+pub const HEART_SPHERE: &str = include_str!("../wgsl/samples/HeartSphere.wgsl");
 
 include!(concat!(env!("OUT_DIR"), "/shaders.rs"));
